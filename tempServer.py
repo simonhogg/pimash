607aae19b0a262298d2a5f445702312aeefb2d8b
shogg <simon@hogg.us>
2014-02-01 10:46:13 -0600

    first attempt at ajax

--- tempServer.py
+++ tempServer.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import simpleTemp
 app = Flask(__name__)
+
+setpoint = 155.0
 
 @app.route("/")
 def index():
 	tempData = {
 		'temp' :  '%.1f' % simpleTemp.getTemp()
+		'setpoint' : '%.1f' % setpoint
 	}
 	return render_template('main.html', **tempData)
+
+@app.route('/_refresh')
+def refresh():
+	tempData = {
+		'temp' :  '%.1f' % simpleTemp.getTemp()
+		'setpoint' : '%.1f' % setpoint
+	}
+	return jsonify(tempdata)
 
 if __name__ == "__main__":
 	app.run(host='10.0.1.33', port=80, debug=True)
