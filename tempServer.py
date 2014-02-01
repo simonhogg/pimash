ffd718924c26827642175add99419876adc839be
shogg <simon@hogg.us>
2014-01-31 20:12:39 -0600

    added call to html file

--- tempServer.py
+++ tempServer.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return repr(simpleTemp.getTemp())
+	tempData = {
+		'temp' : repr(simpleTemp.getTemp())
+	}
+	return render_template('main.html', **tempData)
 
 if __name__ == "__main__":
 	app.run(host='10.0.1.33', port=80, debug=True)
