10a7266c4d8e2e2a4cd45de2ebca0b8b76558fcb
simonhogg <simon@hogg.us>
2014-06-30 17:33:36 -0500

    Added periodic timer (untested)
    Added some experimental code to get an update loop running

--- tornadoServer.py
+++ tornadoServer.py
@@ -3,49 +3,59 @@ import tornado.web
 import tornado.escape
 import tornado.template
 
+import random
 import os.path
 
 from pimashio import PimashIO
 
 # Variables
-temp = 72.0
-setpoint = 80.0
-element = False
+
+
+g = dict(
+    temperature = 72.0
+    setpoint = 80.0
+    element_status = '~'
+    )
 
 io = PimashIO()
 
 # App Logic
 
-def get_tempdata():
+def check_temp():
+    global g
     try:
         # get the current temperature
-        t = io.get_temp()
+        g.temperature = io.get_temp()
         # check if we need to turn the element or not
-        if t > setpoint:
+        if temp > setpoint:
             io.element_off()
-            e = 'Off'
+            g.element_status = 'Off'
         else:
             io.element_on()
-            e = 'On'
-        # create the tempdata packet
-        tempdata = {'temp' :  '%.1f' % t,
-            'setpoint' : '%.1f' % setpoint,
-            'element' : e
-            }
-        print(tempdata)
+            g.element_status = 'On'
     except:
-        print("Error getting temperature data")
-        tempdata = {'temp' :  '%.1f' % 0,
-        'setpoint' : '%.1f' % setpoint,
-        'element' : 'Error'
+        g.temperature = random.randint(72,212)+0.1
+        g.element_status = "Error"
+        
+        
+
+def get_tempdata():
+    global g
+    # create the tempdata packet
+    tempdata = {'temp' :  '%.1f' % g.temperature,
+        'setpoint' : '%.1f' % g.setpoint,
+        'element' : g.element_status
         }
+    print(tempdata)
     return tempdata
 
 # Server Code
 class MainHandler(tornado.web.RequestHandler):
+    global g
     def get(self):
+        global g
        #self.render('main.html', temp=temp, setpoint=setpoint, element=element)
-       self.render('ngindex.html', temp=temp, setpoint=setpoint, element=element)
+       self.render('ngindex.html', temp=g.temperature, setpoint=g.setpoint, element=g.element_status)
 
 class RefreshHandler(tornado.web.RequestHandler):
     def get(self):
@@ -73,4 +83,9 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     application.listen(80)
-    tornado.ioloop.IOLoop.instance().start()
+    main_loop = tornado.ioloop.IOLoop.instance()
+    # Create a periodic timer for our control loop
+    scheduler = tornado.ioloop.PeriodicCallback(check_temp, 1000, io_loop = main_loop)
+    # Start the timer and then the IOloop
+    scheduler.start()
+    main_loop.start()
