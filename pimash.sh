a0fee9c532c4eef7bd35503c022f31ebd99801f4
simonhogg <simon@hogg.us>
2015-02-11 19:49:33 -0600

    pointed at tornadoServer rather than Flask

--- pimash.sh
+++ pimash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo python ~pi/pimash/tempServer.py &
+sudo python ~pi/pimash/tornadoServer.py &
 midori -e Fullscreen -e Navigationbar -e Menubar -e Bookmarkbar -a http://brewery.local &
